a2b8e3482015ed7aba0a24f5369a74fc5b4d2cef
wray1955 <80387170+wray1955@users.noreply.github.com>
2021-06-18 14:28:04 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '671611704a054f52ae6dc3c575b11289',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '中规智绘',
+  domain: 'https://www.notion.so/671611704a054f52ae6dc3c575b11289',
+  author: 'WRay from CAUPD',
 
   // open graph metadata (optional)
   description: 'Example site description',
